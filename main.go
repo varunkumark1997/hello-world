037176bbd357ffa51062148588f01f15dfe6f258
Varun Kumar K <varun.k@razorpay.com>
2022-01-26 19:34:27 +0530

    add random food

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -16,10 +18,23 @@ func main() {
 
 func homepage(w http.ResponseWriter, r *http.Request) {
 
+	size := 650
+	sizeString := strconv.Itoa(size)
+	randx := rand.Int() % size
+	randy := rand.Int() % size
+
 	html := `
-<h1>Hello World!</h1>
+<h2>Snake Game!!</h1>
+
 <div class="container">
-  <canvas id="c" width="650" height="650" style="background:#000"></canvas>
-</div>`
+  <canvas id="myCanvas" width=` + sizeString + ` height=` + sizeString + ` style="background:#000"></canvas>
+</div>
+
+<script>
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+	ctx.fillStyle = 'white';
+	ctx.fillRect(` + strconv.Itoa(randx) + `, ` + strconv.Itoa(randy) + `, 10, 10);  
+</script>`
 	io.WriteString(w, html)
 }
